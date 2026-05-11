14e67d7797314d8baa61caa01116e796c1e47977
Aayush <76879468+aayush-kumar-2310@users.noreply.github.com>
2026-05-11 22:39:09 +0530

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    private boolean isValid(int x, int y, int a, int b, int[][] grid){
+        if(x<0 || x>=a)
+            return false;
+        if(y<0 || y>=b)
+            return false;
+
+        if(grid[x][y] != 1)
+            return false;
+        
+        return true;
+    }
+
+    private int solve(int[][] grid, int x, int y){
+        if(!isValid(x, y, grid.length, grid[0].length, grid))
+            return 0;
+
+        grid[x][y]++;
+
+        return 1 + solve(grid, x+1, y) + solve(grid, x, y+1) + solve(grid, x-1, y) + solve(grid, x, y-1);
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int ans = 0;
+
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    ans = Math.max(ans, solve(grid, i, j));
+                }
+            }
+        }
+
+        return ans;
+    }
+}
